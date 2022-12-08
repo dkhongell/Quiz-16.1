32a91f86885aeb8e68be5590db30fdd2c618853d
dkhongell <116841735+dkhongell@users.noreply.github.com>
2022-12-08 16:41:19 -0500

    Add files via upload
    not as bad as the next one is going to be

---
+++ Q16.py
@@ -0,0 +1,17 @@
+import dbm
+
+photo_category= open.dbm
+photo_category["pets.png", "friends.png", "critters.png"]
+pets.png="dogs, cats, birds"
+friends.png="Sam, Elise, Jess"
+critters.png="ant, roach, worm"
+
+close.dbm
+
+open.photo_category
+
+pets.png="hamster"
+friends.png="Riley"
+critters.png="moth"
+
+print(values)
